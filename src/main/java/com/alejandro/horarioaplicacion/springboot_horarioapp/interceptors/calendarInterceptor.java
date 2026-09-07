039dcdf61aad96769964c139e62104a2eb0f660e
Alejandro <alejmac2508@gmail.com>
2026-09-07 00:03:52 -0600

    se crearon las validaciones de los interceptores

--- src/main/java/com/alejandro/horarioaplicacion/springboot_horarioapp/interceptors/calendarInterceptor.java
+++ src/main/java/com/alejandro/horarioaplicacion/springboot_horarioapp/interceptors/calendarInterceptor.java
@@ -1,16 +1,19 @@
 package com.alejandro.horarioaplicacion.springboot_horarioapp.interceptors;
 
+import java.sql.Date;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView; 
 
-import io.micrometer.core.instrument.distribution.StepBucketHistogram;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
 
 @Component 
 public class calendarInterceptor implements HandlerInterceptor{
@@ -25,21 +28,37 @@ public class calendarInterceptor implements HandlerInterceptor{
             throws Exception {
       
         Calendar calendar = Calendar.getInstance();
-        int hour  = calendar.get(calendar.HOUR_OF_DAY);       
+        Integer hour  = calendar.get(calendar.HOUR_OF_DAY);       
         
-        if(hour >= open && hour<< close){
-            StringBuilder message = new StringBuilder("bienvnidos aal horario de atencion a clientes");
-            message.append(", atendemos desde las");
+        if(hour >= open && hour <= close){
+            StringBuilder message = new StringBuilder("bienvnidos aal horario de atencion a clientes ");
+            message.append(", atendemos desde las ");
             message.append(open);
-            message.append("hoas");
-            message.append("hasta las ");
+            message.append(" hoas ");
+            message.append(" hasta las ");
             message.append(close);
-            message.append("GRACIAS POR LA VISITA");
-            
+            message.append(" GRACIAS POR LA VISITA ");
+            request.setAttribute(" message", message.toString());
             return true;
         }
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String,String> data = new HashMap<>();
+        StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atencion");
+        message.append("por favor visitenos desde las");
+        message.append(open);
+        message.append("y las ");
+        message.append(close);
+        message.append("hrs, Gracias !");
+        data.put("message", message.toString());
+        data.put("day", new Date(0).toString());
+        response.setContentType("aplication/json");
+        response.setStatus(404);
+        response.getWriter().write(mapper.writeValueAsString(data));
         return false;
     }
+
+
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
